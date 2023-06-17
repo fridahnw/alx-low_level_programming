de91ee5018a3e7fd21f24bad2062f0fde7104622
fridahnw <ngimafridah09@gmail.com>
2023-06-17 12:32:01 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point of a program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		if (lc != 'e' && lc != 'q')
+		{
+			putchar(lc);
+		}
+	}
+	putchar('n\');
+	return (0);
+}
