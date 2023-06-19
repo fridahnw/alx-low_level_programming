61390094700bfdc37ef65f779aad808aac14df3e
fridahnw <ngimafridah09@gmail.com>
2023-06-19 04:20:28 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,13 +12,13 @@ int main(void)
 {
 	int p, q;
 
-	for (p = 0; p <= 8; p++)
+	for (p = 0; p <= 9; p++)
 	{
-		putchar('0' + p);
 
 		for (q = p + 1; q <= 9 ; q++)
 		{
-			putchar(q + '0');
+			putchar('0' + p);
+			putchar('0' + q);
 
 			if (p != 8 && q != 9)
 			{
