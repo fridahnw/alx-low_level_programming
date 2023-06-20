f9e5e8846fd0121eafc34765eb8e4946ad4e86c0
fridahnw <ngimafridah09@gmail.com>
2023-06-20 06:19:38 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+/**
+ * main - Entry point of a program
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+
+{
+	printf("_putchar\n");
+
+	return (0);
+}
