9be45057778a94d176cdf752b8ea2a4b5a1d9a01
fridahnw <ngimafridah09@gmail.com>
2023-06-21 11:50:02 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a:  integer to be added
+ * @b:  integer to be added
+ *
+ * Return:0 on success
+
+int add(int a, int b)
+
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
