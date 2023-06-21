c7400f373df8239b31e098b560888b481092dc5a
fridahnw <ngimafridah09@gmail.com>
2023-06-21 00:57:51 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - Entry point of a program
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - prints the alpha in lowercase
+ */
+
+void print_alphabet(void)
+
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+
+	{
+		_putchar('0' + lc);
+	}
+	_putchar('\n');
+}
