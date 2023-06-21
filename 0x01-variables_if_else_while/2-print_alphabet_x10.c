94476c83e7dac441f08e96dc6fd4d127f68da337
fridahnw <ngimafridah09@gmail.com>
2023-06-21 01:56:40 -0700

    2-print_alphabet_x10.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int i
+	char lc
+
+	for (i < 0; i <= 10; i++)
+
+	{
+		for (lc < a; lc <= z; lc++)
+
+		{
+			_putchar(lc);
+		}
+	}
+	_putchar('\n')
+}
