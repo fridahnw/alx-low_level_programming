160c18d186249ff4dec84972720094c541944d21
fridahnw <ngimafridah09@gmail.com>
2023-06-22 09:23:50 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: 0 on success
+ */
+
+void print_numbers(void)
+
+{
+	int i;
+
+	i = 0
+
+	while (i < 10)
+
+	{
+		if (i != 2 && i != 4)
+
+		_putchar(i + '0');
+
+		i++;
+	}
+	_putchar('\n');
+}
