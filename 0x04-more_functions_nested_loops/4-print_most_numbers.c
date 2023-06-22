bcb5fd517764ead0e78b9851d7fd701a9fc29123
fridahnw <ngimafridah09@gmail.com>
2023-06-22 09:29:49 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,18 +7,21 @@
 void print_numbers(void)
 
 {
-	int i;
-
-	i = 0
+	int i = 0;
 
 	while (i < 10)
 
 	{
 		if (i != 2 && i != 4)
 
+		{
+
 		_putchar(i + '0');
+		}
 
 		i++;
 	}
 	_putchar('\n');
+
+	return (0);
 }
