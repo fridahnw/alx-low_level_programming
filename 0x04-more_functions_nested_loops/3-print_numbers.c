59c5a015d98767a101844429736b70a5875107e9
fridahnw <ngimafridah09@gmail.com>
2023-06-22 08:59:13 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: 0 on success
+ */
+
+void print_numbers(void)
+
+{
+	int i;
+
+	int 0;
+
+	while (i < 10)
+
+	{
+		_putchar(i + '0');
+
+		i++;
+	}
+	_putchar('\n');
+}
