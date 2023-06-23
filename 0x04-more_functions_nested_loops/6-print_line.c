d9b99074c41a3c4c0c215dd4a510cbe25c0ee131
fridahnw <ngimafridah09@gmail.com>
2023-06-23 05:17:09 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line n times.
+ *
+ * @n: times straight line is printed.
+ * Return: no return.
+ */
+
+void print_line(int n)
+
+{
+	int i;
+
+	if (n <= 0)
+
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i < n; i++)
+
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
