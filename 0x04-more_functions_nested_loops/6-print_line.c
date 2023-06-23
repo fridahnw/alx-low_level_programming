a167f3703f930d6dd2a73e0584bae790528f85e2
fridahnw <ngimafridah09@gmail.com>
2023-06-23 05:36:26 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -18,10 +18,14 @@ void print_line(int n)
 		_putchar('\n');
 	}
 
-	for (i = 0; i < n; i++)
+	else
 
 	{
-		_putchar('_');
+		for (i = 0; i < n; i++)
+
+		{
+			_putchar('_');
+		}
 	}
 
 	_putchar('\n');
