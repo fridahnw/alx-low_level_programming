4156f4eb56d2517b04b72b6511f0de3a9f2bb2a3
fridahnw <ngimafridah09@gmail.com>
2023-06-26 23:38:43 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,7 +12,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-oid print_array(int *a, int n);
+void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 #endif /* MAIN_H */
