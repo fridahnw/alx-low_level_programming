6dbced1e175b1b907b9c10dbf4d19d1945e89061
fridahnw <ngimafridah09@gmail.com>
2023-07-15 06:46:17 -0700

    k

--- 0x0B-malloc_free/2.c
+++ 0x0B-malloc_free/2.c
@@ -1,19 +1,42 @@
 #include "main.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
 
 /**
  * _strdup -  returns a pointer to a newly allocated space in memory
  *
  * @str: the string
- * 
- * @Return: pointer
+ *
+ * @Return: pointer to the duplicated string
  */
 
 char *_strdup(char *str)
 {
-	if (str == NULL);
+	int i;
+
+	char *f;
+
+	if (str == NULL)
 	{
-		return (Null);
+		return (NULL);
 	}
-	S = 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		;
+	}
+	f = malloc((i + 1) * sizeof(char));
+	if (f == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			f[i] = str[i];
+		}
+	}
+	f[i] = '\0';
+	return (f);
+}
