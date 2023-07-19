2d9186fd0cc9684285c92b4f50eb3c9bd31e0118
fridahnw <ngimafridah09@gmail.com>
2023-07-19 07:03:16 -0700

    p

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array && action)
+	if (array != NULL && action != NULL)
 		for (i = 0; i < size; i++)
 		{
 			(*action)(array[i]);
