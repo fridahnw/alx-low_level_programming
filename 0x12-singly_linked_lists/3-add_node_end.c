dc78ac944e7dc261b5122aaaf4055053fd00b693
fridahnw <ngimafridah09@gmail.com>
2023-07-28 09:26:19 -0700

    k

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_node_end -  adds a new node at the end
+ *
+ * @head: beginning
+ * @str: what to be added
+ * Return: new node
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+
