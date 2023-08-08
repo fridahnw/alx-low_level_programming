5e709aeb91cea27d27622348d5f23f4841d877c4
fridahnw <ngimafridah09@gmail.com>
2023-08-08 04:30:14 -0700

    p

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 		{
-			i = i + 1;
+			;
 		}
 		writen = write(fd, text_content, i);
 		if (writen <= 0)
